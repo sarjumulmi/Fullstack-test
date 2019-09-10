c46adba39c1ea43a57f79fbdadd3f3a0a4b5ba15
Sarju Mulmi <mulmi.sarju@gmail.com>
2019-09-10 12:49:08 -0400

    helper functions and tests added. Part 4a answers added

--- models/Blog.js
+++ models/Blog.js
@@ -21,7 +21,8 @@ const blogSchema = new mongoose.Schema({
   },
   likes: {
     type: Number,
-    min: 0
+    min: 0,
+    default: 0
   }
 })
 
---
+++ utils/list_helper.js
@@ -0,0 +1,49 @@
+const _ = require('lodash')
+
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => (
+  blogs.reduce((prevValue, currentBlog) => {
+    return prevValue + currentBlog.likes
+  }, 0)
+)
+
+const favoriteBlog = (blogs) => (
+  blogs.reduce((prevBlog, currentBlog) => {
+    if (prevBlog.likes && prevBlog.likes >= currentBlog.likes) {
+      return prevBlog
+    } else {
+      return currentBlog
+    }
+  }, {})
+)
+
+const mostBlogs = (blogs) => {
+  const authorCount = _.countBy(blogs, (blog) => blog.author)
+  return Object.keys(authorCount).reduce((prevValue, currentValue) => {
+    if (authorCount[prevValue] >= authorCount[currentValue]) {
+      return { author: prevValue, blogs: authorCount[prevValue] }
+    } else {
+      return { author: currentValue, blogs: authorCount[currentValue] }
+    }
+  }, {})
+}
+ 
+const mostLikes = (blogs) => {
+  const authorWithLikes = _(blogs).groupBy('author')
+    .map((objs, key) => ({
+      'author': key,
+      'likes': _.sumBy(objs, 'likes') }))
+    .value()
+  return _.maxBy(authorWithLikes, 'likes')
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
+}
